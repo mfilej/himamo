3f62250fa6494e61e9ec24cf5e065e2c6eca5851
Miha Filej <miha@filej.net>
2016-04-18 11:33:11 +0200

    Extract assert_all_in_delta

--- test/himamo/baum_welch_test.exs
+++ test/himamo/baum_welch_test.exs
@@ -1,5 +1,6 @@
 defmodule Himamo.BaumWelchTest do
   use ExUnit.Case
+  import TestHelpers.AllInDelta
   alias Himamo.{BaumWelch, Model}
 
   def a do
@@ -188,24 +189,5 @@ defmodule Himamo.BaumWelchTest do
     ] |> Enum.into(Map.new)
 
     assert_all_in_delta(b, expected, 5.0e-9)
-  end
-
-  def assert_all_in_delta(actual, expected, delta \\ 1.0e-10)
-  def assert_all_in_delta(actual, expected, delta)
-    when tuple_size(actual) == length(expected) do
-    actual = Tuple.to_list(actual)
-
-    Enum.zip(actual, expected)
-    |>Enum.each(fn {{actual_p1, actual_p2}, {expected_p1, expected_p2}} ->
-      assert_in_delta(actual_p1, expected_p1, delta)
-      assert_in_delta(actual_p2, expected_p2, delta)
-    end)
-  end
-  def assert_all_in_delta(%Himamo.Matrix{map: map} = _actual, expected, delta)
-    when map_size(map) == map_size(expected) do
-    Enum.each(map, fn({position, entry}) ->
-      expected_value = Map.fetch!(expected, position)
-      assert_in_delta(entry, expected_value, delta)
-    end)
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,24 @@
 ExUnit.start()
+
+defmodule TestHelpers.AllInDelta do
+  import ExUnit.Assertions, only: [assert_in_delta: 3, assert_in_delta: 4]
+
+  def assert_all_in_delta(actual, expected, delta \\ 1.0e-10)
+  def assert_all_in_delta(actual, expected, delta)
+    when tuple_size(actual) == length(expected) do
+    actual = Tuple.to_list(actual)
+
+    Enum.zip(actual, expected)
+    |>Enum.each(fn {{actual_p1, actual_p2}, {expected_p1, expected_p2}} ->
+      assert_in_delta(actual_p1, expected_p1, delta)
+      assert_in_delta(actual_p2, expected_p2, delta)
+    end)
+  end
+  def assert_all_in_delta(%Himamo.Matrix{map: map} = _actual, expected, delta)
+    when map_size(map) == map_size(expected) do
+    Enum.each(map, fn({position, entry}) ->
+      expected_value = Map.fetch!(expected, position)
+      assert_in_delta(entry, expected_value, delta)
+    end)
+  end
+end
