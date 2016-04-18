caed99c1b707c8819f5a82edcfa8af4909516f18
Miha Filej <miha@filej.net>
2016-04-18 11:33:21 +0200

    Extract E-step for Baum-Welch

---
+++ lib/himamo/baum_welch/step_e.ex
@@ -0,0 +1,171 @@
+defmodule Himamo.BaumWelch.StepE do
+  @moduledoc ~S"""
+  Defines the E-step of the Baum-Welch algorithm.
+
+  Calculates required statistics for the given model.
+  """
+
+  alias Himamo.{Matrix, Model}
+
+  @doc ~S"""
+  Computes alpha variable for Baum-Welch.
+
+  `α_t(i)` is the probability of being in state `S_i` at time `t` after
+  observing the first `t` symbols.
+
+  Returns tuple of tuples (size `T×N`) where:
+  * `T` - length of observation sequence
+  * `N` - number of states in the model
+  """
+  @spec compute_alpha(Model.t, list(Model.symbol)) :: tuple
+  def compute_alpha(%Model{a: a, b: b, pi: pi, n: num_states}, observations) do
+    b_map = Model.ObsProb.new(b, observations)
+    observations = List.to_tuple(observations)
+    obs_size = tuple_size(observations)
+
+    states_range = 0..num_states-1
+
+    # initialization
+    first_row = Enum.map(states_range, fn j ->
+      Model.Pi.get(pi, j) * Model.ObsProb.get(b_map, {j, 0})
+    end)
+    |> List.to_tuple
+
+    # induction
+    {result, _} = Enum.map_reduce((1..obs_size-1), first_row, fn(t, prev_row) ->
+      new_row = Enum.map(states_range, fn j ->
+        sum = Stream.map(states_range, fn i ->
+          elem(prev_row, i) * Model.A.get(a, {i, j})
+        end)
+        |> Enum.sum
+
+        rhs = Model.ObsProb.get(b_map, {j, t})
+
+        sum * rhs
+      end)
+      |> List.to_tuple
+
+      {new_row, new_row}
+    end)
+
+    [first_row | result] |> List.to_tuple
+  end
+
+  @doc ~S"""
+  Computes beta variable for Baum-Welch.
+
+  `ß_t(i)` is the probability of being in state `S_i` at time `t` and
+  observing the partial sequence from `t+1` to the end.
+
+  Returns tuple of tuples (size `T×N`) where:
+  * `T` - length of observation sequence
+  * `N` - number of states in the model
+  """
+  @spec compute_beta(Model.t, list(Model.symbol)) :: tuple
+  def compute_beta(%Model{a: a, b: b, n: num_states}, observations) do
+    b_map = Model.ObsProb.new(b, observations)
+    obs_size = length(observations)
+
+    states_range = 0..num_states-1
+
+    # initialization
+    last_row = Tuple.duplicate(1, num_states)
+
+    # induction
+    Enum.reduce((obs_size-2)..0, [last_row], fn(t, [prev_row|_] = rows) ->
+      new_row = Enum.map(states_range, fn i ->
+        Stream.map(states_range, fn j ->
+          transition_prob = Model.A.get(a, {i, j})
+          emission_prob = Model.ObsProb.get(b_map, {j, t+1})
+          prev_beta = elem(prev_row, j)
+          transition_prob * emission_prob * prev_beta
+        end)
+        |> Enum.sum
+      end)
+      |> List.to_tuple
+
+      [new_row | rows]
+    end)
+    |> List.to_tuple
+  end
+
+  @doc ~S"""
+  Computes xi variable for Baum-Welch.
+
+  `ξ_t(i,j)` is the probability of being in state `S_i` at time `t` and in
+  state `S_j` at time `t+1` given the full observation sequence.
+
+  Returns tuple of tuples of tuples (size `T×N×N`) where:
+  * `T` - length of observation sequence
+  * `N` - number of states in the model
+  """
+  @spec compute_xi(Model.t, list(Model.symbol)) :: Matrix.t
+  def compute_xi(%Model{a: a, b: b, n: num_states} = model, observations) do
+    b_map = Model.ObsProb.new(b, observations)
+    obs_size = length(observations)
+
+    alpha = compute_alpha(model, observations)
+    beta = compute_beta(model, observations)
+
+    states_range = 0..num_states-1
+
+    map_states_2d = fn(fun) ->
+      Stream.flat_map(states_range, fn(i) ->
+        Enum.map(states_range, fn(j) ->
+          fun.({i, j})
+        end)
+      end)
+    end
+
+    Enum.flat_map((0..obs_size-2), fn(t) ->
+      denominator = map_states_2d.(fn({i, j}) ->
+        curr_alpha = alpha |> elem(t) |> elem(i)
+        curr_a = Model.A.get(a, {i, j})
+        curr_b_map = Model.ObsProb.get(b_map, {j, t+1})
+        curr_beta = beta |> elem(t+1) |> elem(j)
+
+        curr_alpha * curr_a * curr_b_map * curr_beta
+      end)
+      |> Enum.sum
+
+      map_states_2d.(fn({i, j}) ->
+        curr_alpha = alpha |> elem(t) |> elem(i)
+        curr_a = Model.A.get(a, {i, j})
+        curr_b_map = Model.ObsProb.get(b_map, {j, t+1})
+        curr_beta = beta |> elem(t+1) |> elem(j)
+        numerator = curr_alpha * curr_a * curr_b_map * curr_beta
+
+        {{t, i, j}, numerator/denominator}
+      end)
+    end)
+    |> Enum.into(Matrix.new({obs_size-1, num_states, num_states}))
+  end
+
+  @doc ~S"""
+  Computes gamma variable for Baum-Welch.
+
+  `γ_t(i)` is the probability of being in state `S_i` at time `t` given the
+  full observation sequence.
+
+  Returns tuple of tuples (size `T×N`) where:
+  * `T` - length of observation sequence
+  * `N` - number of states in the model
+  """
+  @spec compute_gamma(Model.t, list(Model.symbol)) :: Matrix.t
+  def compute_gamma(%Model{n: num_states} = model, observations) do
+    xi = compute_xi(model, observations)
+    obs_size = length(observations)
+
+    Enum.flat_map(0..obs_size-2, fn(t) ->
+      Enum.map(0..num_states-1, fn(i) ->
+        sum = Enum.map(0..num_states-1, fn(j) ->
+          Matrix.get(xi, {t, i, j})
+        end)
+        |> Enum.sum
+
+        {{t, i}, sum}
+      end)
+    end)
+    |> Enum.into(Matrix.new({obs_size-1, num_states}))
+  end
+end
--- test/himamo/baum_welch_test.exs
+++ test/himamo/baum_welch_test.exs
@@ -23,123 +23,6 @@ defmodule Himamo.BaumWelchTest do
     |> put({1, 2}, 0.1)
   end
 
-  test "compute_alpha" do
-    model = %Model{
-      a: a,
-      b: b,
-      pi: Model.Pi.new([0.7, 0.3]),
-      n: 2,
-      m: 2,
-    }
-
-    observations = [0, 1, 1, 2, 1, 0, 1]
-    alpha = BaumWelch.compute_alpha(model, observations)
-
-    assert_all_in_delta(alpha, [
-      {0.21,  0.24},
-      {0.1026,  0.0108},
-      {0.021384,  0.004212},
-      {0.00664848,  0.00089748},
-      {0.001439046,  0.000274914},
-      {0.00033325506,  0.00048248784},
-      {0.00019025763,  1.8155081e-05},
-    ])
-  end
-
-  test "compute_beta" do
-    model = %Model{
-      a: a,
-      b: b,
-      pi: Model.Pi.new([0.7, 0.3]),
-      n: 2,
-      m: 2,
-    }
-
-    observations = [0, 1, 1, 2, 1, 0, 1]
-    beta = BaumWelch.compute_beta(model, observations)
-
-    assert_all_in_delta(beta, [
-       {0.00041202932,  0.00050786063},
-       {0.001799348,  0.002203667},
-       {0.0077948,  0.0099071},
-       {0.026528,  0.035702},
-       {0.1292,  0.0818},
-       {0.22,  0.28},
-       {1.0,  1.0},
-    ])
-  end
-
-  test "compute_xi" do
-    model = %Model{
-      a: a,
-      b: b,
-      pi: Model.Pi.new([0.7, 0.3]),
-      n: 2,
-      m: 2,
-    }
-
-    observations = [0, 1, 1, 2, 1, 0, 1]
-    xi = BaumWelch.compute_xi(model, observations)
-    expected = [
-      {{0, 0, 0}, 0.3263493600},
-      {{0, 0, 1}, 0.0888180140},
-      {{0, 1, 0}, 0.5594560500},
-      {{0, 1, 1}, 0.0253765750},
-      {{1, 0, 0}, 0.6907178000},
-      {{1, 0, 1}, 0.1950876200},
-      {{1, 1, 0}, 0.1090607000},
-      {{1, 1, 1}, 0.0051338846},
-      {{2, 0, 0}, 0.6532516300},
-      {{2, 0, 1}, 0.1465268700},
-      {{2, 1, 0}, 0.1930061600},
-      {{2, 1, 1}, 0.0072153385},
-      {{3, 0, 0}, 0.7418791900},
-      {{3, 0, 1}, 0.1043786000},
-      {{3, 1, 0}, 0.1502196900},
-      {{3, 1, 1}, 0.0035225234},
-      {{4, 0, 0}, 0.2734296900},
-      {{4, 0, 1}, 0.6186691900},
-      {{4, 1, 0}, 0.0783536270},
-      {{4, 1, 1}, 0.0295474960},
-      {{5, 0, 0}, 0.2878227100},
-      {{5, 0, 1}, 0.0639606030},
-      {{5, 1, 0}, 0.6250660900},
-      {{5, 1, 1}, 0.0231505960},
-    ] |> Enum.into(Map.new)
-
-    assert_all_in_delta(xi, expected, 5.0e-9)
-  end
-
-  test "compute_gamma" do
-    model = %Model{
-      a: a,
-      b: b,
-      pi: Model.Pi.new([0.7, 0.3]),
-      n: 2,
-      m: 2,
-    }
-
-    observations = [0, 1, 1, 2, 1, 0, 1]
-    gamma = BaumWelch.compute_gamma(model, observations)
-
-    expected = [
-      {{0, 0}, 0.41516738},
-      {{0, 1}, 0.58483262},
-      {{1, 0}, 0.88580541},
-      {{1, 1}, 0.11419459},
-      {{2, 0}, 0.79977850},
-      {{2, 1}, 0.20022150},
-      {{3, 0}, 0.84625779},
-      {{3, 1}, 0.15374221},
-      {{4, 0}, 0.89209888},
-      {{4, 1}, 0.10790112},
-      {{5, 0}, 0.35178331},
-      {{5, 1}, 0.64821669},
-    ] |> Enum.into(Map.new)
-
-     assert_all_in_delta(gamma, expected, 5.0e-9)
-  end
-
   test "reestimate_a" do
     model = %Model{
       a: a,
@@ -150,8 +33,8 @@ defmodule Himamo.BaumWelchTest do
     }
 
     observations = [0, 1, 1, 2, 1, 0, 1]
-    xi = BaumWelch.compute_xi(model, observations)
-    gamma = BaumWelch.compute_gamma(model, observations)
+    xi = BaumWelch.StepE.compute_xi(model, observations)
+    gamma = BaumWelch.StepE.compute_gamma(model, observations)
 
     a = BaumWelch.reestimate_a(2, length(observations), xi: xi, gamma: gamma)
 
@@ -175,7 +58,7 @@ defmodule Himamo.BaumWelchTest do
     }
 
     observations = [0, 1, 1, 2, 1, 0, 1]
-    gamma = BaumWelch.compute_gamma(model, observations)
+    gamma = BaumWelch.StepE.compute_gamma(model, observations)
 
     b = BaumWelch.reestimate_b(model, observations, gamma: gamma)
 
