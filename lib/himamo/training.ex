11e70b2d1a8a7d9b6ca3c7633b873da8680f869f
Miha Filej <miha@filej.net>
2016-05-03 14:27:19 +0200

    Mofify Training to multiply obs seq probabilities

--- lib/himamo/training.ex
+++ lib/himamo/training.ex
@@ -1,7 +1,7 @@
 defmodule Himamo.Training do
   def perform(initial_model, obs_seq, epsilon) do
-    {initial_stats, initial_prob} = compute_stats(initial_model, obs_seq)
-    perform({initial_model, initial_stats, initial_prob}, obs_seq, epsilon, 100, 1.0)
+    {initial_stats_list, initial_prob} = compute_stats_list(initial_model, obs_seq)
+    perform({initial_model, initial_stats_list, initial_prob}, obs_seq, epsilon, 100, 1.0)
   end
   def perform(result, _, epsilon, _, delta) when delta < epsilon do
     IO.puts "done (delta (#{delta}) < epsilon(#{epsilon})"
@@ -13,9 +13,9 @@ defmodule Himamo.Training do
     result
   end
 
-  def perform({model, stats, prob}, obs_seq, epsilon, iter_left, _) do
-    new_model = Himamo.BaumWelch.reestimate_model(model, obs_seq, stats)
-    {new_stats, new_prob} = compute_stats(new_model, obs_seq)
+  def perform({model, stats_list, prob}, obs_seq, epsilon, iter_left, _) do
+    new_model = Himamo.BaumWelch.reestimate_model(model, stats_list)
+    {new_stats, new_prob} = compute_stats_list(new_model, obs_seq)
 
     delta = abs(prob - new_prob)
 
@@ -24,9 +24,12 @@ defmodule Himamo.Training do
     perform({new_model, new_stats, new_prob}, obs_seq, epsilon, iter_left-1, delta)
   end
 
-  defp compute_stats(model, obs_seq) do
-    new_stats = Himamo.BaumWelch.compute_stats(model, obs_seq)
-    new_prob = Himamo.ForwardBackward.compute(new_stats.alpha)
-    {new_stats, new_prob}
+  defp compute_stats_list(model, obs_seq) do
+    new_stats_list = Himamo.BaumWelch.compute_stats_list(model, [obs_seq])
+    new_prob =
+      Stream.map(new_stats_list, fn({_, prob_k, _}) -> prob_k end)
+      |> Enum.reduce(1, fn(prob, product) -> product * prob end)
+
+    {new_stats_list, new_prob}
   end
 end
